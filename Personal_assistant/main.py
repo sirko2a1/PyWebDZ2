5358a25cbffbbfc776fe379bcc5e9bf59616ef0e
sirko2a1 <134325906+sirko2a1@users.noreply.github.com>
2023-10-04 00:07:11 +0300

    commit №2

--- Personal_assistant/main.py
+++ Personal_assistant/main.py
@@ -1,20 +1,34 @@
-from .AdressBook.AB import main as ab_main
-from .NoteBook.NB import main as nb_main
-from .Map.Map import main as map_main
-from .sort.sort import main as sort_main
-from .Game.game import main as game_main
 import os
+from abc import ABC, abstractmethod
+from AdressBook.AB import main as ab_main
+from NoteBook.NB import main as nb_main
+from Map.Map import main as map_main
+from sort.sort import main as sort_main
+from Game.game import main as game_main
 
 def cls():
-    os.system(['clear','cls'][os.name == 'nt'])
+    os.system(['clear', 'cls'][os.name == 'nt'])
 
-def menu():
-    cls()
-    while True:
+class Menu(ABC):
+
+    @abstractmethod
+    def display_menu(self):
+        pass
+
+    @abstractmethod
+    def run_selected_option(self, choice):
+        pass
+
+class ConcreteMenu(Menu):
+
+    def display_menu(self):
         cls()
         print('MENU')
         choice = input(
             'Вітаю, я ваш персональний помічник.\nОберіть функцію:\n1.Записна книжка\n2.Нотатник\n3.Карта\n4.Сортування папки\n5.Гра\n0.Вихід\n>>>')
+        return choice
+
+    def run_selected_option(self, choice):
         if choice == '1':
             cls()
             ab_main()
@@ -30,8 +44,14 @@ def menu():
         elif choice == '5':
             cls()
             game_main()
-        elif choice == '0':
+
+def main():
+    concrete_menu = ConcreteMenu()
+    while True:
+        choice = concrete_menu.display_menu()
+        if choice == '0':
             break
+        concrete_menu.run_selected_option(choice)
 
 if __name__ == '__main__':
-    menu()
+    main()
